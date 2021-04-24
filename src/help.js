83c2b70acea36aeb242ff381e81f2f9413116fc6
hollow16 <83133432+hollow16@users.noreply.github.com>
2021-04-24 21:07:28 +0200

    Update help.js

--- src/help.js
+++ src/help.js
@@ -4,82 +4,10 @@ const help = (prefix) => {
 
 ◪ *Información*
   ❏ Comando: 「  ${prefix}  」
-  ❏ Creador: Shanduy
-  ❏ Como instalar el bot: https://youtu.be/rfxuYsWGmQo
-  ❏ Instagram: https://www.instagram.com/thepavos
+  ❏ Creador: нσllσgяαм
 
 ◪ *Para Usar Al Bot*
   ❏ Registrate con el comando ${prefix}daftar Nombre
-
-◪ *Nueva Funcion*
-  │
-  ├─ ❏ ${prefix}welcome 1
-  └─ Ojo la persona tiene que tener la foto de perfil publica, en futuras actualizaciones la mejoraremos
-
-◪ *SOBRE*
-  │
-  ├─ ❏ ${prefix}ping
-  └─ Estado de tu internet "Broma"
-
-◪ *CREAR*
-  │
-  ├─ ❏ ${prefix}sticker
-  ├─ ❏ ${prefix}stickergif
-  ├─ 6 segundos de video
-  ├─ ❏ ${prefix}toimg
-  ├─ De sticker a JPG
-  ├─ ❏ ${prefix}tomp3
-  └─ De video a MP3
-
-◪ *DESCARGAS*
-  │
-  ├─ ❏ ${prefix}ytmp4
-  └─ Mas link del video de YT
-
-◪ *AUDIO*
-  │
-  ├─ ❏ ${prefix}play
-  ├─ Mas nombre y artista
-  ├─ ❏ ${prefix}bahasa
-  └─ ❏ ${prefix}tts es
-
-◪ *OTROS*
-  │
-  ├─ ❏ ${prefix}wame
-  ├─ Link de Whatsapp
-  ├─ ❏ ${prefix}virtex (Hernqn)
-  ├─ ❏ ${prefix}exe
-  └─ ❏ ${prefix}qrcode
-  
-◪ *GRUPOS*
-  │
-  ├─ ❏ ${prefix}closecg
-  ├─ Cerrar el grupo solo admins
-  ├─ ❏ ${prefix}opencg
-  ├─ Abrir grupo solo admins
-  ├─ ❏ ${prefix}kick
-  ├─ Eliminar a un miembro 
-  ├─ ❏ ${prefix}promote
-  ├─ Dar admin a un miembro
-  ├─ ❏ ${prefix}demote
-  ├─ Quitar el admin
-  ├─ ❏ ${prefix}linkgc
-  ├─ Link del grupo
-  ├─ ❏ ${prefix}tagall
-  └─ Nombra a todos los del grupo
-
-Para usar estas funciones el bot necesita admin
-  
-◪ *NSWF NUEVAS FUNCIONES* 
-  │
-  └─ ❏ ${prefix}nsfwmenu
-
-Para activar los NSFW coloque el siguiente comando ${prefix}nsfw 1 y para desactivar los NSFW coloque ${prefix}nsfw 0
-
-No te olvides de seguirme en instagram flaco :)
-
-Para saber la ultima version mas actual del bot entra en 
-https://github.com/shanduy/ShanBot
 
 by Shanduy
 `
